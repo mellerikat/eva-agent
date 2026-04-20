45fd4840cda43c1e2d037ccb8514d815a10a8f0a
taehoon2.park <taehoon2.park@lge.com>
2026-04-20 09:43:38 +0000

    update: install script default chart version to 2.7.0

--- install_eva_agent.sh
+++ install_eva_agent.sh
@@ -18,7 +18,7 @@ Usage:
 
 Options:
   --chart <chart>                   Helm chart reference (default: eva-agent/eva-agent)
-  --chart-version <ver>             Helm chart version (latest if omitted)
+  --chart-version <ver>             Helm chart version (default: 2.7.0)
   --image <tag>                     Image tag (defaults to values.yaml image.tag)
   --namespace <ns>                  Namespace (default: eva-agent)
   --context <ctx>                   Kube context (default: current context)
@@ -60,7 +60,7 @@ AWS_ECR_HOST="${AWS_ECR_HOST:-339713051385.dkr.ecr.ap-northeast-2.amazonaws.com}
 AWS_PROFILE="${AWS_PROFILE:-}"
 ECR_REPO_NAME="${ECR_REPO_NAME:-mellerikat/release/eva-agent}"
 CHART="${CHART:-eva-agent/eva-agent}"
-CHART_VERSION="${CHART_VERSION:-}"
+CHART_VERSION="${CHART_VERSION:-2.7.0}"
 IMAGE_TAG="${IMAGE_TAG:-}"
 CHECK_DIGEST="${CHECK_DIGEST:-0}"
 BASE_DIR="${BASE_DIR:-$(pwd)}"
